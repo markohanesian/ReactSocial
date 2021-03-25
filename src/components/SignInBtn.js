f520b4035fb88576227ccf3e1d8921a1646c737b
markohanesian <mso872@gmail.com>
2021-03-25 14:52:36 -0700

    created SignInBtn component

---
+++ src/components/SignInBtn.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const btnStyle = {
+    backgroundColor: 'salmon',
+    color: 'white',
+    border: 'none',
+    padding: '5px',
+
+    // backgroundImage: 'url(' + imgUrl + ')',
+  };
+
+export default function SignInBtn() {
+    return (
+        <div>
+            <button style={btnStyle}>Sign In With Google</button>
+            hello
+        </div>
+    )
+}
