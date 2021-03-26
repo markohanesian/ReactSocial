515fd3ddac1313463565942f2a5752827f4e3033
markohanesian <mso872@gmail.com>
2021-03-25 18:01:04 -0700

    Added user avatar display in navbar after sign in

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { UserContext } from '../contexts/user'
 import SignInBtn from "./SignInBtn"
 
 const navStyle = {
@@ -8,11 +9,24 @@ const navStyle = {
     padding: '12px 24px',
     backgroundColor: 'white'
 }
+
+const NavbarImgStyle = {
+    height: '40px',
+    width: '40px',
+    borderRadius: '50%'
+}
+
 export default function NavBar() {
+
+    const [user, setUser] = useContext(UserContext).user
+
+
+
     return (
         <div style={navStyle}>
             <p>ReactSocial</p>
-            <SignInBtn />
+            {/* if user is signed in, display avatar in navbar */}
+            {user ? <img alt={"avatar"} style={NavbarImgStyle} src={user.photoURL} />: <SignInBtn />}
         </div>
     )
 }
