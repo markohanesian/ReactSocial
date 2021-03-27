e940005e718ad307648cd91955607b4d328cd64d
markohanesian <mso872@gmail.com>
2021-03-27 15:38:21 -0700

    Added text area and upload button

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { UserContext } from '../contexts/user'
 import SignInBtn from "./SignInBtn"
 
+// topmost div
 const CreatePostStyle = {
     display: 'flex',
     alignItems: 'center',
@@ -8,15 +10,58 @@ const CreatePostStyle = {
     padding: '2rem',
 }
 
+// Sub-div containing text and other elements
+const CreatePostContainer = {
+
+}
+
+// "Create a Post"
 const PromptStyle = {
     marginLeft: '1rem'
 }
 
+// User post textarea
+const CreatePostTextArea = {
+
+}
+
+// upload image areaa
+const CreatePostUploadImage = {
+
+}
+
+
 export default function CreatePost() {
+    const [user, setUser] = useContext(UserContext).user;
+    const [caption, setCaption] = useState("");
+    const handleChange = () => {
+        
+    }
     return (
         <div style={CreatePostStyle}>
-            <SignInBtn />
-            <p style={PromptStyle}>to post and comment</p>
+
+            {/* if user exists, post. If not, sign in */}
+            {user ? (
+
+                <div style={CreatePostContainer}>
+                    <p style={PromptStyle}>Create Post</p>
+                    <div style={CreatePostTextArea}>
+                        <textarea
+                            rows="3"
+                            value={caption}
+                            onChange={((e) => setCaption(e.target.value))}
+                        >
+                        </textarea>
+                    </div>
+                    <div style={CreatePostUploadImage}>
+                        <input type="file" accept="image/*" onChange={handleChange} />
+                    </div>
+                </div>
+            ) :
+                (<div>
+                    <SignInBtn />
+                </div>
+                )}
         </div>
     )
 }
