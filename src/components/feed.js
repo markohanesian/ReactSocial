201816832445c318db4279f720b90b8fc3403a55
markohanesian <mso872@gmail.com>
2021-04-01 13:54:40 -0700

    Added feed component to home

---
+++ src/components/feed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Post } from './post';
+
+const FeedStyle = {
+
+}
+
+export default function feed() {
+    return (
+        <div style={FeedStyle}>
+            <Post />
+        </div>
+    )
+}
