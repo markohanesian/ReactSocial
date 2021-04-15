e9d0809c2049ae80540b5658497a179f3f7017d3
markohanesian <mso872@gmail.com>
2021-04-15 12:46:26 -0700

    Created NewsList component to retrieve article data

---
+++ src/components/NewsList.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+// NewsItem component to display new articles
+import NewsItem from './NewsItem';
+
+const NewsList = () => {
+    // When no articles, nothing will be shown
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        const getArticles = async () => {
+            const res = await axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=701157776827481988e23accca272ebc");
+            // show retrieved data in console
+            
+            
+            setArticles(res.data.articles);
+            console.log(res)
+        };
+        getArticles();
+    },
+        // empty array so that data is fetched only once
+        [])
+
+    
+    return (
+        <div>
+            {articles.map(({title, description, url, urlToImage}) => (
+                <NewsItem
+                    title={title}
+                    description={description}
+                    url={url}
+                    urlToImage={urlToImage}
+                />
+            ))}
+        </div>
+    )
+}
+
+export default NewsList;
