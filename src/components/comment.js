304af694ac29b3cad5f88fedb9c85755a7c3ee35
markohanesian <mso872@gmail.com>
2021-04-28 22:29:38 -0700

    Updated styling

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const CommentStyle = {
     display: 'flex',
-    flexDirection: 'row',
-    marginLeft: '3rem',
-    padding: '1rem'
+    flexDirection: 'column',
+    marginLeft: '1rem',
+    padding: '1rem',
+    backgroundColor: 'whitesmoke'
 }
 
 const CommentUser = {
+    display: 'flex',
     fontWeight: '600',
     marginRight: '1rem'
 }
 
 const CommentCaption = {
-    
+    display: 'flex',
+    padding: '.5rem 0 0 0'
 }
 
-export default function Comment({username, caption}) {
+export default function Comment({ username, caption }) {
     return (
         <div style={CommentStyle}>
             <p style={CommentUser}>{username}</p>
