95b2e2012d2a9ba48ef0bb7d2bd4aca217ae4881
markohanesian <mso872@gmail.com>
2021-04-28 23:04:27 -0700

    Added FooterBar to pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Route,
 } from "react-router-dom";
 import NavBar from './components/NavBar';
+import FooterBar from './components/FooterBar';
 // pages
 import Home from "./components/home";
 import News from "./components/news";
@@ -30,14 +31,17 @@ export default function App() {
             <Route path="/about">
               <NavBar />
               <About />
+              <FooterBar />
             </Route>
             <Route path="/news">
               <NavBar />
               <News />
+              <FooterBar />
             </Route>
             <Route path="/">
               <NavBar />
               <Home />
+              <FooterBar />
             </Route>
           </Switch>
         </div>
