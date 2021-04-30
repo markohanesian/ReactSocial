4eff5e5d31473ebcc41042b5b6d77a4a522e3a75
markohanesian <mso872@gmail.com>
2021-04-29 23:04:48 -0700

    Updated info

--- src/components/about.js
+++ src/components/about.js
@@ -22,10 +22,10 @@ export default function about() {
                 about
             </h1> */}
             <p style={AboutParagraph}>
-                I created this social media web application in order to be able to stay in touch with friends and family and also have a place to check out the latest news.
+                I created ReactSocial as a way to have my own custom social feed online where I can interact with friends and family by sharing images and posting about various topics for fun.
             </p>
             <p style={AboutParagraph}>
-                I plan to add more features to make the site more interactive, fun, and secure, but in the meantime, sign in and create a post or check out the latest top news headlines.
+                I plan to add more features to make the site more interactive, fun, and secure, but in the meantime, sign in and create a post or comment.
             </p>
             <br></br>
             <Link href="https://github.com/markohanesian/social-media-app">View Source Code on GitHub</Link>
