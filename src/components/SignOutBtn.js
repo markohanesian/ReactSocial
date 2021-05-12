61f9b394d0dfaa92535b6f92c214ab70ec5f7a85
markohanesian <mso872@gmail.com>
2021-05-11 17:47:01 -0700

    Created SignOutBtn to sign users out

---
+++ src/components/SignOutBtn.js
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import { UserContext } from '../contexts/user';
+import { logout } from "../services/auth";
+import { useHistory } from 'react-router-dom';
+
+
+const btnStyle = {
+    backgroundColor: 'salmon',
+    color: 'white',
+    border: 'none',
+    padding: '5px',
+    borderRadius: '5px',
+    marginRight: '1rem'
+
+    // backgroundImage: 'url(' + imgUrl + ')',
+};
+
+export default function SignOutBtn() {
+    let history = useHistory();
+    const [user, setUser] = useContext
+        (UserContext).user;
+
+    // sign in function
+    const SignOutBtnClick = async () => {
+        let SignedOutUser = await logout();
+        if (SignedOutUser) setUser(SignedOutUser);
+            console.log(user);
+        localStorage.removeItem('token');
+        history.push('/signed-out');
+    };
+
+    return (
+        <div>
+            <button style={btnStyle} onClick={SignOutBtnClick}>
+                Sign Out
+            </button>
+        </div>
+    )
+}
