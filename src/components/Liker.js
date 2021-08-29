30891d2507dc0da165d0334ff0d500ce03043719
mso872 <mso872@gmail.com>
2021-08-29 13:54:14 -0700

    Fixed post button likes

--- src/components/Liker.js
+++ src/components/Liker.js
@@ -1,27 +1,39 @@
 import React, { useState } from 'react';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 
-const ThumbUpButton = {
+const ThumbUpButtonClicked = {
   backgroundColor: 'rgb(115, 250, 179)',
+  order: 'none',
+  padding: '5px',
+  borderRadius: '5px',
+  margin: '1rem',
+  fontSize: '2rem'
+}
+
+const ThumbUpButton = {
+  backgroundColor: 'whitesmoke',
   border: 'none',
   padding: '5px',
   borderRadius: '5px',
-}
-
-const ThumbStyle = {
-  color: 'white'
+  margin: '1rem',
+  fontSize: '2rem'
 }
 
 const Liker = () => {
-  const [thumb, setThumbUp] = useState();
+  const [thumb, setThumbUp] = useState(false);
 
   return (
-    <div>
-      <button style={ThumbUpButton} onClick={setThumbUp}>
-        {thumb ? <ThumbUpIcon style={ThumbStyle} /> : <ThumbUpIcon style={ThumbUpButton}/>}
-        
+    <>
+      <button
+        style={{
+          border: 'none',
+          backgroundColor: 'transparent'
+        }}
+        onClick={() => setThumbUp(thumb => !thumb)}
+      >
+        <ThumbUpIcon style={thumb ? ThumbUpButtonClicked : ThumbUpButton} />
       </button>
-    </div>
+    </>
   );
 }
 
