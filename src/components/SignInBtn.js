3ca79511eaf7b724e51fe1ccdb828437e32121a2
mso872 <mso872@gmail.com>
2024-02-08 17:15:34 -0800

    Add icon to sign in button

--- src/components/SignInBtn.js
+++ src/components/SignInBtn.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { UserContext } from '../contexts/user';
 import { signInWithGoogle } from "../services/auth";
 import { Button } from '@material-ui/core';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 export default function SignInBtn() {
     const [user, setUser] = useContext
@@ -16,7 +17,7 @@ export default function SignInBtn() {
 
     return (
         <div>
-            <Button className='sign-in-btn' onClick={signInBtnClick} variant="contained" style={{background: "#8bc34a", color: "black", fontWeight: "bold", marginRight: "1rem"}}>
+            <Button className='sign-in-btn' onClick={signInBtnClick} variant="contained" endIcon={<ArrowForwardIcon />} style={{background: "#8bc34a", color: "black", fontWeight: "bold", marginRight: "1rem"}}>
                 Sign In
             </Button>
         </div>
