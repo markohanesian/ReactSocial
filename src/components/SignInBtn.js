3d5928eb55ff45b91c14f9ddc0bcae0290cd9b01
mso872 <mso872@gmail.com>
2024-02-08 17:26:42 -0800

    Make sign in button wide 100%

--- src/components/SignInBtn.js
+++ src/components/SignInBtn.js
@@ -1,25 +1,36 @@
-import React, { useContext } from 'react';
-import { UserContext } from '../contexts/user';
+import React, { useContext } from "react";
+import { UserContext } from "../contexts/user";
 import { signInWithGoogle } from "../services/auth";
-import { Button } from '@material-ui/core';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { Button } from "@material-ui/core";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 export default function SignInBtn() {
-    const [user, setUser] = useContext
-        (UserContext).user;
+  const [user, setUser] = useContext(UserContext).user;
 
-    // sign in function
-    const signInBtnClick = async () => {
-        let userBySignIn = await signInWithGoogle();
-        if (userBySignIn) setUser(userBySignIn);
-        console.log(user);
-    };
+  // sign in function
+  const signInBtnClick = async () => {
+    let userBySignIn = await signInWithGoogle();
+    if (userBySignIn) setUser(userBySignIn);
+    console.log(user);
+  };
 
-    return (
-        <div>
-            <Button className='sign-in-btn' onClick={signInBtnClick} variant="contained" endIcon={<ArrowForwardIcon />} style={{background: "#8bc34a", color: "black", fontWeight: "bold", marginRight: "1rem"}}>
-                Sign In
-            </Button>
-        </div>
-    )
+  return (
+    <div style={{ width: "100%" }}>
+      <Button
+        className="sign-in-btn"
+        onClick={signInBtnClick}
+        variant="contained"
+        endIcon={<ArrowForwardIcon />}
+        style={{
+          background: "#8bc34a",
+          color: "black",
+          fontWeight: "bold",
+          marginRight: "1rem",
+          width: "100%",
+        }}
+      >
+        Sign In
+      </Button>
+    </div>
+  );
 }
