62fe0c722b035d582cd6cfe734292f2134707fe1
mso872 <mso872@gmail.com>
2024-07-09 21:58:02 -0700

    Add about back to Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,39 +1,56 @@
-import React, { useContext } from 'react';
-import { UserContext } from '../contexts/user';
+import React, { useContext } from "react";
+import { UserContext } from "../contexts/user";
 import SignInBtn from "./SignInBtn";
 import { Link } from "react-router-dom";
-import ProfileMenu from './ProfileMenu';
+import ProfileMenu from "./ProfileMenu";
 
 const navStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%',
-    backgroundColor: '#101010',
-    padding: "1rem"
-}
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  width: "100%",
+  backgroundColor: "#101010",
+  padding: "1rem",
+};
 
 const LogoStyle = {
-    padding: '0 1rem 0 1rem',
-    fontSize: '1.2rem',
-    fontWeight: '800',
-    textDecoration: 'none',
-    color: 'rgb(139, 195, 74)',
-}
+  padding: "0 1rem 0 1rem",
+  fontSize: "1.2rem",
+  fontWeight: "800",
+  textDecoration: "none",
+  color: "rgb(139, 195, 74)",
+};
+
+const AboutStyle = {
+    padding: "0 1rem 0 1rem",
+    fontSize: "1rem",
+    fontWeight: "800",
+    textDecoration: "none",
+    color: "white",
+  };
 
 export default function NavBar() {
-    const [user] = useContext(UserContext).user;
-    return (
-        <div style={navStyle}>
-            <div>
-                <Link to="/" style={LogoStyle}>ReactSocial</Link>
-            </div>
-            {/* if user is signed in, display profile menu in navbar */}
-            {user ? 
-                <>
-                    <ProfileMenu />
-                </>:
-                <div style={{ width: "110px", marginRight: '1rem'}}><SignInBtn /></div>}
+  const [user] = useContext(UserContext).user;
+  return (
+    <div style={navStyle}>
+      <div>
+        <Link to="/" style={LogoStyle} alt="navigate to home">
+          ReactSocial
+        </Link>
+        <Link style={AboutStyle} to="about" alt="navigate to about">
+          About
+        </Link>
+      </div>
+      {/* if user is signed in, display profile menu in navbar */}
+      {user ? (
+        <>
+          <ProfileMenu />
+        </>
+      ) : (
+        <div style={{ width: "110px", marginRight: "1rem" }}>
+          <SignInBtn />
         </div>
-    );
+      )}
+    </div>
+  );
 }
