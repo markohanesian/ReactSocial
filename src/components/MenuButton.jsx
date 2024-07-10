9a15f4cb9e99f5338eb14bc9ce74f6c0c0e5444b
mso872 <mso872@gmail.com>
2024-07-09 22:54:16 -0700

    Update MenuButton to allow href for Links

--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -1,56 +1,33 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const MenuButton = ({ text, onClick, to, alt }) => {
+const MenuButton = ({ text, onClick, to, alt, href }) => {
   return (
-    <>
-      {to ? (
-        <Button
-          variant="text"
-          component={Link}
-          to={to}
-          onClick={onClick}
-          aria-label={alt}
-          sx={{
-            color: "white",
-            "&.MuiButton-text": {
-              color: "white",
-            },
-            "&:hover": {
-              color: "rgb(139, 195, 74)",
-            },
-            "&.MuiButtonbase-root": {
-              display: "flex",
-            },
-          }}
-        >
-          {text}
-        </Button>
-      ) : (
-        <Button
-          variant="text"
-          onClick={onClick}
-          aria-label={alt}
-          sx={{
-            color: "white",
-            "&.MuiButton-text": {
-              color: "white",
-            },
-            "&:hover": {
-              color: "rgb(139, 195, 74)",
-            },
-            "&.MuiButtonbase-root": {
-              display: "flex",
-              alignItems: "flex-start"
-            },
-          }}
-        >
-          {text}
-        </Button>
-      )}
-    </>
+    <Button
+      variant="text"
+      component={href ? 'a' : (to ? RouterLink : 'button')}
+      to={to}
+      href={href}
+      onClick={onClick}
+      aria-label={alt}
+      sx={{
+        color: "white",
+        "&.MuiButton-text": {
+          color: "white",
+        },
+        "&:hover": {
+          color: "rgb(139, 195, 74)",
+        },
+        "&.MuiButtonBase-root": {
+          display: "flex",
+          alignItems: "flex-start",
+        },
+      }}
+    >
+      {text}
+    </Button>
   );
 };
 
@@ -59,6 +36,7 @@ MenuButton.propTypes = {
   onClick: PropTypes.func,
   to: PropTypes.string,
   alt: PropTypes.string.isRequired,
+  href: PropTypes.string,
 };
 
 export default MenuButton;
