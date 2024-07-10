176ac17326706be0479f32bd47de22baecf97c38
mso872 <mso872@gmail.com>
2024-07-10 11:23:26 -0700

    Upgrade Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,24 @@
-import firebase from 'firebase';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// Required for side-effects
+import "firebase/firestore";
 
- // Your web app's Firebase configuration
- const firebaseConfig = {
+// Your web app's Firebase configuration
+const firebaseConfig = {
   apiKey: "AIzaSyCy49QJAEVPPURcw1wPnrR5yaF0Gu-GFlc",
   authDomain: "social-media-app-mso.firebaseapp.com",
   projectId: "social-media-app-mso",
   storageBucket: "social-media-app-mso.appspot.com",
   messagingSenderId: "267213391628",
-  appId: "1:267213391628:web:a17b05c10a4cd2b9192b29"
+  appId: "1:267213391628:web:a17b05c10a4cd2b9192b29",
 };
 // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const provider = new GoogleAuthProvider();
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
-const storage = firebase.storage();
-const provider = new firebase.auth.GoogleAuthProvider();
-
-export {db, auth, storage, provider};
+export { app, auth, db, storage, provider };
