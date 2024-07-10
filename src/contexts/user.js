f0463bf4c2d1fe971af21e336cae476659720f8b
mso872 <mso872@gmail.com>
2024-07-10 11:24:00 -0700

    Fix bugs in user object

--- src/contexts/user.js
+++ src/contexts/user.js
@@ -1,33 +1,31 @@
 import React, { createContext, useState, useEffect } from "react";
-import { auth } from "../firebase"; 
-import firebase from 'firebase/app';
+import { auth } from "../firebase";
+import { onAuthStateChanged, setPersistence, browserLocalPersistence } from "firebase/auth";
 
 export const UserContext = createContext();
 
 export const UserContextProvider = (props) => {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        // Set persistence to local
-        auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-            .then(() => {
-                // Add the auth state listener
-                auth.onAuthStateChanged((user) => {
-                    if (user) {
-                        setUser(user);
-                    } else {
-                        setUser(null);
-                    }
-                });
-            })
-            .catch((error) => {
-                console.error("Error setting persistence:", error);
-            });
-    }, []);
+  useEffect(() => {
+    setPersistence(auth, browserLocalPersistence)
+      .then(() => {
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+            setUser(user);
+          } else {
+            setUser(null);
+          }
+        });
+      })
+      .catch((error) => {
+        console.error("Error setting persistence:", error);
+      });
+  }, []);
 
-    return (
-        <UserContext.Provider value={{ user: [user, setUser] }}>
-            {props.children}
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider value={{ user: [user, setUser] }}>
+      {props.children}
+    </UserContext.Provider>
+  );
 };
