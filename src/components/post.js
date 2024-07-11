3ac2d517da7ab05d0a23a435d07ae4555332a7c4
mso872 <mso872@gmail.com>
2024-07-11 15:46:35 -0700

    Disable the console log to clear the console

--- src/components/post.js
+++ src/components/post.js
@@ -67,7 +67,7 @@ export default function Post({
   const userEmail = user && user.length > 0 ? user[0].email : null;
 
   // console.log("Owner Email:", ownerEmail); // Log ownerEmail to verify in the browser console
-  console.log("User email:", userEmail); // Log user to verify in the browser console
+  // console.log("User email:", userEmail); // Log user to verify in the browser console
 
   const handleDelete = async () => {
     try {
